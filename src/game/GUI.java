aff24a21791d1c97a6f7e7f7044399e701bba851
DrJonias <100142633+DrJonias@users.noreply.github.com>
2022-03-26 13:18:41 +0100

    first power-up
    upgrade button welcher den score pro click erhöht. kann nur gekauft werden, wenn man genügend score hat.

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -9,6 +9,7 @@ public class GUI  extends JFrame implements ActionListener{
 
 	JLabel score = new JLabel();
 	JButton clicker = new JButton("press");
+	JButton upgrade = new JButton("upgrade");
 	
 	
 	Logic logic;
@@ -19,10 +20,14 @@ public class GUI  extends JFrame implements ActionListener{
 
 		this.logic = logic;
 		
+		
 		add(score, BorderLayout.NORTH);
 		
 		clicker.addActionListener(this);
 		add(clicker, BorderLayout.CENTER);
+		
+		upgrade.addActionListener(this);
+		add(upgrade, BorderLayout.SOUTH);
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 800);
@@ -33,11 +38,22 @@ public class GUI  extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getSource() == clicker) {
-			logic.addScore(1);
-			score.setText(String.valueOf(logic.score));
+			logic.addScore(logic.power);
+			refreshScore();
+		}
+		else if(e.getSource() == upgrade) {
+			logic.addPower(1, 10);
+			refreshScore();
 		}
 		
 	
 	}
+	public void refreshScore() {
+		score.setText(String.valueOf(logic.score));
+	}
 	
+	public void notEnoughScore(int amount) {
+		String st = "You don't have enough score. You need " + amount + " more score";
+		JOptionPane.showMessageDialog(null, st);
+	}
 }
--- src/game/Logic.java
+++ src/game/Logic.java
@@ -4,6 +4,7 @@ public class Logic {
 	GUI g = new GUI(this);
 
 	protected int score = 0;
+	protected int power = 1;
 	
 	public void addScore(int amount) {
 		score += amount;
@@ -11,5 +12,15 @@ public class Logic {
 	
 	public void removeScore(int amount) {
 		score -= amount;
+	}
+	
+	public void addPower(int amount, int price) {
+		if(price <= score) {
+			power += amount;
+			removeScore(price);
+		}
+		else {
+			g.notEnoughScore(price-score);
+		}
 	}
 }
