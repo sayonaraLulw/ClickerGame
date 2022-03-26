7d1e75a4ddc0a3e0af930e8d4501252c5edae454
Oscar Lam <lam.oscar@outlook.com>
2022-03-26 15:28:10 +0100

    Update GUI_Shop.java

--- src/game/GUI_Shop.java
+++ src/game/GUI_Shop.java
@@ -1,3 +1,10 @@
+/* Created by Oscar
+ * 
+ * ToDo:
+ * - Score update on score change (after buying power up)
+ * 
+ * */
+
 package game;
 
 import javax.swing.*;
@@ -9,37 +16,37 @@ import java.awt.event.ActionListener;
 public class GUI_Shop extends JFrame implements ActionListener{
 	// Variables
 	JLabel shop = new JLabel();
-	JLabel puplvl1 = new JLabel("Level 1", SwingConstants.CENTER);
-	JLabel puplvl2 = new JLabel("Level 1", SwingConstants.CENTER);
-	JLabel puplvl3 = new JLabel("Level 1", SwingConstants.CENTER);
+	JLabel pupLvl1 = new JLabel("Level 1", SwingConstants.CENTER);
+	JLabel pupLvl2 = new JLabel("Level 1", SwingConstants.CENTER);
+	JLabel pupLvl3 = new JLabel("Level 1", SwingConstants.CENTER);
 	JButton powerup1 = new JButton("Powerup 1");
 	JButton powerup2 = new JButton("Powerup 2");
 	JButton powerup3 = new JButton("Powerup 3");
 	JButton closeShop = new JButton("Close shop");
-	JPanel shopelements = new JPanel();
+	JPanel shopElements = new JPanel();
 	
 	public GUI_Shop(int score) {
 		// Set layout
 		setLayout(new BorderLayout());
-		shopelements.setLayout(new GridLayout(3, 2));
+		shopElements.setLayout(new GridLayout(3, 2));
 		
 		// JLabel
 		shop = new JLabel("Shop - Score: " + score);
 		add(shop, BorderLayout.NORTH);
 		
 		// Shop elements
-		shopelements.add(powerup1);
-		shopelements.add(puplvl1);
-		shopelements.add(powerup2);
-		shopelements.add(puplvl2);
-		shopelements.add(powerup3);
-		shopelements.add(puplvl3);
+		shopElements.add(powerup1);
+		shopElements.add(pupLvl1);
+		shopElements.add(powerup2);
+		shopElements.add(pupLvl2);
+		shopElements.add(powerup3);
+		shopElements.add(pupLvl3);
 		
 		// ActionListener
 		closeShop.addActionListener(this);
 		
 		// Adds
-		add(shopelements, BorderLayout.CENTER);
+		add(shopElements, BorderLayout.CENTER);
 		add(closeShop, BorderLayout.SOUTH);
 		
 		// Swing defaults
